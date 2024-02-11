cd4463a33f926f88f34fbdba65328e2cacba8a65
Cindy Leochico <dakisleochico@gmail.com>
2024-02-11 00:21:04 -0700

    create bonus exercise

---
+++ script.js
@@ -0,0 +1,22 @@
+let images = [
+  "https://images.unsplash.com/photo-1682687219640-b3f11f4b7234?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1706463629335-d92264bbfd6f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1682695796497-31a44224d6d6?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+let imageTag = document.querySelector(".imageTag");
+let i = 0;
+
+function next() {
+  if (i >= images.length - 1) {
+    return false;
+  }
+  i++;
+  imageTag.setAttribute("src", images[i]);
+}
+function previous() {
+  if (i <= 0) {
+    return false;
+  }
+  i--;
+  imageTag.setAttribute("src", images[i]);
+}
